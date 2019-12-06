a9e5210921ad22a9d908597cfafa2c343c876abc
aok10000 <44146090+aok10000@users.noreply.github.com>
2019-12-06 08:55:19 -0800

    Update and rename index.md to bruh.java

---
+++ bruh.java
@@ -0,0 +1,33 @@
+package Fun;
+
+
+public class CoolBoomer
+{   public static void main(String[] args)
+    {
+        while(true){
+            try
+                {
+                Thread.sleep(70);
+                for(int i = 0; i < 76; i++)
+                {
+                    System.out.print("-");Thread.sleep(5);
+                    
+                }
+                
+                Thread.sleep(70);
+                System.out.println("\n┏━━━┓ ┏┓┏━┓   ┏━━┓︱ ┏━━━┓ ┏━━━┓ ┏━┓┏━┓ ┏━━━┓ ┏━━━┓");
+                Thread.sleep(70);
+                System.out.println("┃┏━┓┃ ┃┃┃┏┛   ┃┏┓┃︱ ┃┏━┓┃ ┃┏━┓┃ ┃┃┗┛┃┃ ┃┏━━┛ ┃┏━┓┃");
+                Thread.sleep(70);
+                System.out.println("┃┃︱┃┃ ┃┗┛┛︱   ┃┗┛┗┓ ┃┃︱┃┃ ┃┃︱┃┃ ┃┏┓┏┓┃ ┃┗━━┓ ┃┗━┛┃");
+                Thread.sleep(70);
+                System.out.println("┃┃︱┃┃ ┃┏┓┃︱   ┃┏━┓┃ ┃┃︱┃┃ ┃┃︱┃┃ ┃┃┃┃┃┃ ┃┏━━┛ ┃┏┓┏┛");
+                Thread.sleep(70);
+                System.out.println("┃┗━┛┃ ┃┃┃┗┓   ┃┗━┛┃ ┃┗━┛┃ ┃┗━┛┃ ┃┃┃┃┃┃ ┃┗━━┓ ┃┃┃┗┓");
+                Thread.sleep(70);
+                System.out.println("┗━━━┛ ┗┛┗━┛   ┗━━━┛ ┗━━━┛ ┗━━━┛ ┗┛┗┛┗┛ ┗━━━┛ ┗┛┗━┛");
+                }
+                catch(InterruptedException e){}
+        }
+    }
+}
